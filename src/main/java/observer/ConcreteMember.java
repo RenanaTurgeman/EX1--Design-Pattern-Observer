6db80be362599a2141d5921d73e7bd1507e3e5c3
Renana Turgeman <renanaturgeman058@gmail.com>
2022-12-21 17:31:29 +0200

    add document

--- src/main/java/observer/ConcreteMember.java
+++ src/main/java/observer/ConcreteMember.java
@@ -3,10 +3,13 @@ package observer;
 public class ConcreteMember implements Member{
     UndoableStringBuilder undoableStringBuilder;
 
-
+    /**
+     *this method responsible to update the observers  about the action that happen
+     * @param usb- the action that happen
+     */
     @Override
     public void update(UndoableStringBuilder usb) {
-        this.undoableStringBuilder = usb;
+        undoableStringBuilder = usb;
     }
 
     @Override
--- src/main/java/observer/GroupAdmin.java
+++ src/main/java/observer/GroupAdmin.java
@@ -3,53 +3,88 @@ package observer;
 import java.util.ArrayList;
 
 public class GroupAdmin implements Sender {
-     ArrayList<Member> cotumers;
+     ArrayList<Member> costumers;
     UndoableStringBuilder undoableStringBuilder;
+
+    //constructor
     public GroupAdmin(){
-        this.cotumers = new ArrayList<>();
+        this.costumers = new ArrayList<>();
         this.undoableStringBuilder = new UndoableStringBuilder();
     }
 
+    /**
+     * register the member obj - add it to the ArrayList costumer
+     * @param obj - the member we register
+     */
     @Override
     public void register(Member obj) {
-        if(!cotumers.contains(obj)){
-        this.cotumers.add(obj);
+        if(!costumers.contains(obj)){
+            costumers.add(obj);
         }
+        notifyObservers();
     }
 
+    /**
+     * unregister the member obj - remove obj from the ArrayList costumer
+     * @param obj- the member we unregister
+     */
     @Override
     public void unregister(Member obj) {
-        if(cotumers.contains(obj)){
-            this.cotumers.remove(obj);
+        if(costumers.contains(obj)){
+            costumers.remove(obj);
         }
+        notifyObservers();
     }
 
+    /**
+     *  the function inserts the string into this character sequence.
+     * @param offset - the position where we insert the string
+     * @param obj - the string we insert.
+     */
     @Override
     public void insert(int offset, String obj) {
         undoableStringBuilder.insert(offset,obj);
-        //maby update?
+        notifyObservers();
     }
 
     @Override
     public void append(String obj) {
         undoableStringBuilder.append(obj);
+        notifyObservers();
     }
 
+    /**
+     *Removes the characters in a substring of this sequence.
+     * @param start
+     * @param end
+     */
     @Override
     public void delete(int start, int end) {
-        undoableStringBuilder.delete(start,end);
+        try {
+            undoableStringBuilder.delete(start, end);
+            notifyObservers();
+        }
+        catch (StringIndexOutOfBoundsException error) {
+            System.err.println("StringIndexOutOfBoundsException");
+        }
     }
 
     @Override
     public void undo() {
         undoableStringBuilder.undo();
+        notifyObservers();
     }
 
+    public void notifyObservers(){
+        for(Member costumer : costumers){
+            costumer.update(undoableStringBuilder);
+        }
+    }
 
     @Override
     public String toString() {
         return "GroupAdmin{" +
-                "cotumers=" + cotumers +
+                "cotumers=" + costumers +
                 ", undoableStringBuilder=" + undoableStringBuilder +
                 '}';
     }
