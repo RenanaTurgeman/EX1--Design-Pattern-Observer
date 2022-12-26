538c88d609ddc5d7fdec889a8753c3c477e550bc
Renana Turgeman <renanaturgeman058@gmail.com>
2022-12-26 17:55:33 +0200

    uploud our soulution to EX0

--- src/main/java/observer/UndoableStringBuilder.java
+++ src/main/java/observer/UndoableStringBuilder.java
@@ -1,98 +1,123 @@
 package observer;
 import java.util.Stack;
+import java.util.EmptyStackException;
+
 /*
 Use the class you've implemented in previous assignment
  */
+
 /**
- * StringBuilder with undo support
- * java.lang.StringBuilder - class with modifier <b>final</b>,
- * so no inheritance, use delegation.
+ * A class that we could use it to do undo for certain functions from StringBuilder class
+ *
+ * @author Renana Turgeman and Ofir Shitrit
+ * @version 1.10 06 November 2022
  */
-interface Action{
-    void undo();
-}
+
 public class UndoableStringBuilder {
+    Stack<String> states;
+    StringBuilder sb;
 
-    private StringBuilder stringBuilder; // delegate
-    /**
-     * Operations that are the reverse of those performed.
-     * That is, when append is called, it is placed on the stack
-     * "delete" operation. When calling undo() it
-     * will be executed.    */
-    private Stack<Action> actions = new Stack<>();
 
-    // constructor
     public UndoableStringBuilder() {
-        stringBuilder = new StringBuilder();
+        states = new Stack<>();
+        sb = new StringBuilder();
+
     }
 
-    public UndoableStringBuilder reverse() {
-        stringBuilder.reverse();
-        Action action = new Action(){
-            public void undo() {
-                stringBuilder.reverse();
-            }
-        };
-        actions.add(action);
+
+    /**
+     * This method appends the mentioned string with the existing string.
+     *
+     * @param string - the string that we want append
+     * @return the two strings that connect
+     */
+    UndoableStringBuilder append(String string) {
+        this.states.push(sb.toString());
+        this.sb.append(string);
         return this;
     }
 
-    public UndoableStringBuilder append(String str) {
-        stringBuilder.append(str);
-
-        Action action = new Action(){
-            public void undo() {
-                stringBuilder.delete(
-                        stringBuilder.length() - str.length(),
-                        stringBuilder.length());
-            }
-        };
-        actions.add(action);
+    /**
+     * This method will delete the string from the mentioned start index till the end index.
+     *
+     * @param start - the start index of the string we want to delete.
+     * @param end   - the end index of the string we want to delete.
+     * @return the original string without the substring we delete
+     */
+    UndoableStringBuilder delete(int start, int end) {
+        try {
+            this.states.push(sb.toString());
+            this.sb.delete(start, end);
+        } catch (StringIndexOutOfBoundsException error) {
+            System.err.println("There is a problem, please check the bounds");
+        }
         return this;
     }
 
-    public UndoableStringBuilder insert(int offset, String str) {
-        stringBuilder.insert(offset, str);
-        Action action = new Action(){
-            public void undo() {
-                stringBuilder.delete(offset, offset+str.length());
-            }
-        };
-        actions.add(action);
+    /**
+     * It will insert the mentioned string to the other string from the specified offset position.
+     *
+     * @param offset - the position we want to insert the new string
+     * @param s      - the string we want to insert
+     * @return the original string with the s string inside it.
+     * this.sb.delete(start, end);
+     */
+    UndoableStringBuilder insert(int offset, String s) {
+        try {
+            this.states.push(sb.toString());
+            this.sb.insert(offset, s);
+        } catch (StringIndexOutOfBoundsException error) {
+            System.err.println("There is a problem, please check the bounds");
+        }
         return this;
     }
 
-    public UndoableStringBuilder delete(int start, int end) {
-        String deleted = stringBuilder.substring(start, end);
-        stringBuilder.delete(start, end);
-        Action action = new Action(){
-            public void undo() {
-                stringBuilder.insert(start, deleted);
-            }
-        };
-        actions.add(action);
+    /**
+     * It will reverse the string.
+     *
+     * @return the reverse string
+     */
+    UndoableStringBuilder reverse() {
+        this.states.push(sb.toString());
+        this.sb.reverse();
         return this;
     }
 
-    public UndoableStringBuilder replace(int start, int end, String str) {
-        String deleted = stringBuilder.substring(start, end);
-        stringBuilder.replace(start, end, str);
-        Action action = new Action(){
-            public void undo() {
-                stringBuilder.replace(start, start+str.length(), deleted);
-            }
-        };
-        actions.add(action);
+    /**
+     * It will replace the original string with the specified string
+     * from the start index till the end index.
+     *
+     * @param start the first index that from it we change the characters
+     * @param end   the last index that until it we change the characters
+     * @param s     the string we want to replace with the substring from start to end
+     * @return the original string with the s string that replaced substring
+     */
+    UndoableStringBuilder replace(int start, int end, String s) {
+        try {
+            this.states.push(sb.toString());
+            this.sb.replace(start, end, s);
+        } catch (StringIndexOutOfBoundsException e) {
+            System.err.println("There is a problem, please check the bounds");
+        } catch (NullPointerException e) {
+            System.err.println("There is a problem, please check if the string is not null");
+        }
         return this;
     }
 
-    public void undo(){
-        if(!actions.isEmpty()){
-            actions.pop().undo();
+    /**
+     * undo the last action
+     */
+    public void undo() {
+        try {
+            sb = new StringBuilder(this.states.pop());
+        } catch (EmptyStackException e) {
+            sb = new StringBuilder();
         }
     }
 
+    @Override
     public String toString() {
-        return stringBuilder.toString();
+        return sb.toString();
     }
+
 }
